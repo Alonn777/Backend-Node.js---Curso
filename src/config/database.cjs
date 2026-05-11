76f5aea1401bed541ab2ba713561bb87c3a4727b
CarlosHenrique <henriquevilas764@gmail.com>
2026-05-10 22:01:13 -0300

    Adaptando o sequelize para common js para que a migration de certo ao iniciar o projeto na VPS

---
+++ src/config/database.cjs
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+module.exports = process.env.DATABASE_URL
+  ? {
+      url: process.env.DATABASE_URL,
+      dialect: 'postgres',
+      define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+      }
+    }
+  : {
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+      }
+    }
